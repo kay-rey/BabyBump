80b6b05b09b02cd55794cf7d060f95aa3d244ff5
kay-rey <kevbreyes@gmail.com>
2017-03-18 19:15:58 -0700

    Initial Commit

---
+++ BaybeeBump/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BaybeeBump
+//
+//  Created by Kevin Baltazar Reyes on 3/18/17.
+//  Copyright © 2017 Kevin Baltazar Reyes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
